e3fdcc5c4eb6a094d4495481ad5f0594d3d6d0fb
Tufahel <tufahel96@gmail.com>
2022-09-13 16:31:21 +0600

    db, model, controller, routes and api setup

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root "static#index"
+  namespace :api, defaults: {format: 'json'} do 
+    resources :greetings, only:[:index]
+  end
 end
