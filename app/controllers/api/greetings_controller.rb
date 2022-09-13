5d35267330987d53fdc312ab788e82a1933b7eb4
Tufahel <tufahel96@gmail.com>
2022-09-13 21:06:48 +0600

    linters fixed

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,7 +1,7 @@
 class Api::GreetingsController < ApplicationController
-    def index
-        random_greeting = Greeting.all.sample
-		greet = random_greeting.greeting
-		render json: { greet: }.to_json
-    end
+  def index
+    random_greeting = Greeting.all.sample
+    greet = random_greeting.greeting
+    render json: { greet: }.to_json
+  end
 end
