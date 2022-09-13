02e8e497b96405817c832994d3c13d1e83fc6fd2
Tufahel <tufahel96@gmail.com>
2022-09-13 20:53:23 +0600

    controllers updated

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -2,6 +2,6 @@ class Api::GreetingsController < ApplicationController
     def index
         random_greeting = Greeting.all.sample
 		greet = random_greeting.greeting
-		render json: { greet: }
+		render json: { greet: }.to_json
     end
 end
