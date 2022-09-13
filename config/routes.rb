7533b59dea8667a60e7a24d7cd5cc02faf39a7c6
Tufahel <tufahel96@gmail.com>
2022-09-13 20:54:31 +0600

    update routes and static index to show javascript

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  root "static#index"
   namespace :api, defaults: {format: 'json'} do 
     resources :greetings, only:[:index]
   end
+  # Defines the root path route ("/")
+  get "*page", to: 'static#index'
+  root "static#index"
 end
